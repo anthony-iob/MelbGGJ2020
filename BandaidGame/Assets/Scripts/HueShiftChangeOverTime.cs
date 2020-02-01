06cac771b8b9a2b62106c769199cd0da0a69f97e
Lewis <bluestriker26@hotmail.com>
2020-02-02 10:29:42 +1100

    New player prefab

---
+++ BandaidGame/Assets/Scripts/HueShiftChangeOverTime.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+[ExecuteInEditMode]
+public class HueShiftChangeOverTime : MonoBehaviour
+{
+    public PostProcessProfile profile;
+    private ColorGrading colorGradingSettings;
+
+    public float tempChangeAmount = 2;
+    public float tempChangeSpeed = 2;
+
+    private void Awake()
+    {
+        profile.TryGetSettings(out colorGradingSettings);
+        colorGradingSettings.hueShift.value = 0;
+    }
+
+    void Update()
+    {
+        colorGradingSettings.hueShift.value = tempChangeAmount * Mathf.Sin(tempChangeSpeed * Time.time);
+    }
+
+    private void OnDestroy()
+    {
+        colorGradingSettings.hueShift.value = 0;
+    }
+}
