9037611cadf1f3c25b552d9630cdb0f46ed09aaa
Helen <Helencodemonkey@gmail.com>
2020-02-01 20:42:48 +1100

    fix up managers in main level

---
+++ BandaidGame/Assets/Scripts/Pause.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Pause : Singleton<MonoBehaviour>
+{
+    public UnityEvent pauseGame, resumeGame;
+    bool paused = false;
+    // Start is called before the first frame update
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        paused = true;
+    }
+
+    public void ResumeGame() {
+        Time.timeScale = 1;
+        paused = false;
+    }
+
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Tab) ) {
+            if(paused) {
+                Debug.Log("resuming");
+                resumeGame.Invoke();
+            } else {
+                Debug.Log("pausing");
+                pauseGame.Invoke();
+            }
+        }
+    }
+}
