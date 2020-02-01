1067d2ffeed4ac4d3cb223604298ddc87fa6fd2b
Helen <Helencodemonkey@gmail.com>
2020-02-01 21:43:38 +1100

    fix up pause menu

---
+++ BandaidGame/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ReloadScene();   
+        }
+    }
+
+    public void ReloadScene() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void LoadMainMenu() {
+        SceneManager.LoadScene(SceneManager.GetSceneByName("MainMenu").buildIndex);
+    }
+}
