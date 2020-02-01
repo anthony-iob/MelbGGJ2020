4d16f3af8540e3fab62c23f215d8f188feb5f302
Helen <Helencodemonkey@gmail.com>
2020-02-01 22:29:49 +1100

    Add flood

---
+++ BandaidGame/Assets/Scripts/Flood.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flood : MonoBehaviour
+{
+    public GameObject flood;
+    public Transform minHeight;
+    public Transform maxHeight;
+    // Update is called once per frame
+    void Update()
+    {
+        var minHeightPosition = minHeight.position;
+        var maxHeightPosition = maxHeight.position;
+        var percentage = GameManager.instance.GetFloodPercentage();
+        Debug.Log(percentage);
+        var newHeight = (maxHeightPosition.y - minHeightPosition.y) * percentage;
+        var up = new Vector3(0, newHeight, 0);
+        flood.transform.position = minHeightPosition + up;
+    }
+}
