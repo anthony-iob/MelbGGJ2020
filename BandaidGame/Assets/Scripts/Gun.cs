66e89cae35995a9ee3e4c25c89a007b92f335825
Seamus <hzrdtv@gmail.co>
2020-02-02 14:37:23 +1100

    Auto stash before merge of "master" and "origin/master"

--- BandaidGame/Assets/Scripts/Gun.cs
+++ BandaidGame/Assets/Scripts/Gun.cs
@@ -36,8 +36,8 @@ public class Gun : Singleton<Gun>
         {
             loadedBullet.GetComponent<Bullet>().charged = true;
 			CameraShaker.Instance.ShakeOnce(8f, 2f, 0.1f, 3f);
+            loadedBullet.transform.localScale *= chargeShotSizeMultiplier;
             explosion.Play();
-			chargedBulletFire.Play();
         }
 		CameraShaker.Instance.ShakeOnce(1f, 2f, 0.1f, 2f);
 		bulletFire.PlayOneShot(bulletSFX[Random.Range(0, bulletSFX.Length)]);
