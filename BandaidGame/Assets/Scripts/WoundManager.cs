d5325eeff3185ae518ce11ce6fdf74e4a4c06adf
Seamus <hzrdtv@gmail.co>
2020-02-02 14:28:12 +1100

    fix noise array address

--- BandaidGame/Assets/Scripts/WoundManager.cs
+++ BandaidGame/Assets/Scripts/WoundManager.cs
@@ -76,7 +76,7 @@ public class WoundManager : MonoBehaviour
             Bandaidable wound = GetClosedWound();
             if(wound != null) {
                 wound.bleed.Invoke();
-                audioSource.PlayOneShot(hurtNoises[Random.Range(0, hurtNoises.Length)]);
+                audioSource.PlayOneShot(hurtNoises[Random.Range(0, hurtNoises.Length-1)]);
             }
         }
     }
