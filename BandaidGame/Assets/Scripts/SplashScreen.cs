781daa05db4c6d1e252bc66bcfa12112f600caa8
anthony-iob <anthony.iob7@gmail.com>
2020-02-05 17:09:04 +1100

    Added the Global Game Jam 2020 Splash Screen before the Main Menu

---
+++ BandaidGame/Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+
+    private float delay = 0;
+    public float loadTime = 4;
+    public string sceneToLoad;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (delay < loadTime)
+        {
+            delay += Time.deltaTime;
+        }
+        else
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+
+    }
+}
